3625326f6dcb798070440e96f12289c7e2a67d44
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-09-04 19:49:25 +0530

    added problem model

---
+++ src/models/problem.model.ts
@@ -0,0 +1,65 @@
+import mongoose, { Document } from "mongoose";
+
+export interface ITestCase {
+  input: string;
+  output: string;
+}
+
+export interface IProblem extends Document {
+  title: string;
+  description: string;
+  difficulty: "easy" | "medium" | "hard";
+  createdAt: Date;
+  updatedAt: Date;
+  editorial?: string;
+  testCases: ITestCase[];
+}
+
+const testSchema = new mongoose.Schema<ITestCase>({
+  input: {
+    type: String,
+    required: [true, "Input is required"],
+  },
+  output: {
+    type: String,
+    required: [true, "Output is required"],
+  },
+});
+
+const problemSchema = new mongoose.Schema<IProblem>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      maxLength: [100, "Title must be less than 100 characters"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    difficulty: {
+      type: String,
+      enum: {
+        values: ["easy", "medium", "hard"],
+        message: "Difficulty must be easy, medium or hard",
+      },
+      default: "easy",
+      required: true,
+    },
+    editorial: {
+      type: String,
+      trim: true,
+    },
+    testCases: [testSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+problemSchema.index({ title: 1 }, { unique: true }); // index based on title
+problemSchema.index({ difficulty: 1 }); // index based on difficulty
+
+export const Problem = mongoose.model<IProblem>("Problem", problemSchema);
